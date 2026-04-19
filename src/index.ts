5a7cce96f83098f7158ba7fa7e6e418321b59811
Priyanshu <prysh310704@gmail.com>
2026-04-19 17:33:19 +0530

    Revert "Basic express+ts setup done"
    This reverts commit a5e49e9c6dae04f3144a3816889f145aebf62e52.

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import serverConfig from './config/serverConfig.js'
-const app=express();
-
-app.listen(serverConfig.PORT,()=>{
-    console.log('Server is running at PORT : ',serverConfig.PORT);
-    console.log("WOWWW");
-    
-})
